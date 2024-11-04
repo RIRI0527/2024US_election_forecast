e2be95f0279d884dbb462c071b1390cedb19779d
RIRI0527 <159666740+RIRI0527@users.noreply.github.com>
2024-11-04 16:34:39 -0500

    updated the code and the Preamble
    updated the code and the Preamble

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,27 +1,19 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from fivethirtyeight
+# Author: Ruizi Liu, Yuechen Zhang, Bruce Zhang
+# Date: 4 November 2024
+# Contact: ruizi.liu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Download the data form fivethirtyeight first 
+# Any other information needed? None
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
 
-data <- read_csv(here::here("data/01-raw_data/raw_data.csv"))
-#### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
-
+the_raw_data <- read_csv(here::here("data/01-raw_data/president_polls.csv"))
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(the_raw_data, here::here("data/01-raw_data/raw_data.csv"))
 
          
